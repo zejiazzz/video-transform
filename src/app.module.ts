0276d0bc5e9a046a85915d6c4fedd3484a2fb76f
zhangzejia <1326582859@qq.com>
2026-05-31 11:41:28 +0800

    refactor: clean up app.module.ts by removing ConfigModule import and simplifying imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,10 @@ import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PostService } from './post/post.service';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    UserModule,
-    OrderModule,
-    PrismaModule,
-    PostModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
+  imports: [UserModule, OrderModule, PrismaModule, PostModule],
   controllers: [AppController],
   providers: [AppService, PostService],
 })
